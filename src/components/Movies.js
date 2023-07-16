9dc68bcc642b2eecd4ada467b3fc46fba3e499a8
Smartysidd <Sidd271299@gmail.com>
2023-07-16 23:22:09 +0530

    push

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,29 +5,55 @@ function Movies() {
 
     const [movies , setMovies] = useState([])
     const [pageNum , setpageNum]= useState(1)
+    const [watchList, setWatchList] = useState([])
+    const [hovered , setHovered]=useState('')
+
+
+//Pagination Handlers
+
 
     const onNext=()=>{
-
 setpageNum(pageNum+1)
-    }
-
-
+};
 
     const onPrev=()=>{
 if(pageNum>1){
     setpageNum(pageNum-1)
-}
-    }
+}  
+  };
+                  //watchlist Handlers
 
+                  const addToWatchList=(id)=>{
+                         const newWatchList=[...watchList,id]
+                         setWatchList(newWatchList);
+                  };
+                         const removeFromWatchList = (id) => {
+                         const filteredWatchList=watchList.filter((watchlistID)=>{
+                            return (watchlistID != id);
+                         });
+                         setWatchList( filteredWatchList);
+                        };
+//Hovering on Movie cards//
+
+                        const showButton=(id)=>{
+                            setHovered(id)
+                        }
+
+                        const hideButton=(id)=>{
+                            setHovered('')
+                        }
+                         console.log(watchList)
+                    
+                  
 
     useEffect(() => {
         (function(){  axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=2517cafdf89d5164a6df39d10b62ab27&page=${pageNum}`).then((res) => {
             setMovies(res.data.results);
         });
-    })()
+    })();
       
     },[pageNum])
-console.log(pageNum)
+// console.log(pageNum)
     // console.log(movies)
     return (
         <div>
@@ -37,15 +63,34 @@ console.log(pageNum)
             <div className="flex flex-wrap">
 {
     movies.map((movie)=>{
-        return  <div key={movie.id} 
-        className="w-[200px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h-[40vh] md:w-[200px] hover:scale-110 duration-300 relative flex-items-end" style={{
+        return ( <div 
+            onMouseOver={()=>showButton(movie.id)}
+            onMouseLeave={()=>hideButton()}
+            key={movie.id}  
+        className="w-[200px] h-[35vh] bg-center bg-cover rounded-xl m-4 md:h-[40vh] md:w-[200px] hover:scale-110 duration-300 relative flex-items-end" style={{
             backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`,
         }}>
-            <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-50">
+
+
+<div className="text-2xl p-2 bg-gray=900 rounded-2xl absolute right-2 top-2 "
+style={{display:hovered==movie.id ? 'block' : 'none'}}> 
+             {
+                watchList.includes(movie.id)==false ?(
+                
+<div onClick={()=>addToWatchList(movie.id)}>😊</div>)  
+:(
+    <div onClick={()=>removeFromWatchList(movie.id)}>❌</div>
+    
+)
+    }
+</div>
+
+            <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-60">
                 {/* John Wick */}
                 {movie.title}
             </div>
         </div>
+        );
     })
 }
                 {/* <div className="w-[200px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h-[40vh] md:w-[200px] hover:scale-110 duration-300 relative flex-items-end" style={{
@@ -60,9 +105,10 @@ console.log(pageNum)
 
                 </div> */} 
             </div>
-            <Pagination pageNumProp={pageNum} onNextProp={onNext} onPrevProp={onPrev}/>
+            <Pagination pageNumProp={pageNum} onNextProp={onNext} onPrevProp={onPrev}></Pagination>
         </div>
       
     );
-}
-export default Movies
+            }
+
+export default Movies;
