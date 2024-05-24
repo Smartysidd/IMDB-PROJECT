51761c1a426c0051f093cf4b1b1f583f855652f6
Siddharth Gupta <117941758+Smartysidd@users.noreply.github.com>
2024-05-24 13:59:42 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className='h-[20vh] md:h-[60vh] bg-center bg-no-repeat flex items-end'
     style={{
-        backgroundImage : `url(https://assets-in.bmscdn.com/discovery-catalog/events/et00311714-ewdhvajezf-landscape.jpg)`
+        backgroundImage : `url(https://assets-in.bmscdn.com/discovery-catalog/events/et00311714-tznvsxurde-landscape.jpg)`
     }}>
 
 
